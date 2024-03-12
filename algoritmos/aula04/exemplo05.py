8b6b02539bcaf9f9cd430e703f8495c6f7af8b4b
nordivi <victorw.nordi@icloud.com>
2024-03-12 19:58:51 -0300

    :sparkles: python

---
+++ algoritmos/aula04/exemplo05.py
@@ -0,0 +1,10 @@
+# 1
+x_1 = float(input('Coloque o valor de x1: '))
+x_2 = float(input('Coloque o valor de x2: '))
+y_1 = float(input('Coloque o valor de y1: '))
+y_2 = float(input('Coloque o valor de y2: '))
+dist_x = x_2 - x_1
+dist_y = y_2 - y_1
+dist = (dist_x**2 + dist_y**2)**(1/2)
+print('A distância entre os pontos x e y é ', dist)
+# 5.831
---
+++ algoritmos/aula04/exemplo06.py
@@ -0,0 +1,12 @@
+nascimento = int(input('Ano de nascimento: '))
+idade = 24-nascimento
+print('Idade em anos: ', idade)
+
+meses = idade*12
+print('Idade em meses: ', meses)
+
+dias = 365*idade
+print('Idade em dias: ', dias)
+
+semanas = idade*52
+print('Idade em semanas: ', semanas)
