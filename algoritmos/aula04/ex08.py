aa16666a5959949a2ff43a1a956d0f913b80456d
nordivi <victorw.nordi@icloud.com>
2024-03-18 19:33:14 -0300

    :sparkles: conversao pes

--- algoritmos/aula04/ex08.py
+++ algoritmos/aula04/ex08.py
@@ -1,2 +1,16 @@
 p = float(input('Coloque a medida em pés: '))
 polegadas = p*12
+jardas = p/3
+milhas = jardas/1760
+
+# a
+print(f"{p} pés são {polegadas} polegadas.")
+
+# b
+print(f"{p} pés são {jardas} jardas.")
+
+# c
+print(f"{p} pés são {milhas} milhas.")
+
+
+
