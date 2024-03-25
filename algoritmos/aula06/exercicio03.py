f0e755e661ad2ad4b7a70ad125abc564dce49bb4
nordivi <victorw.nordi@icloud.com>
2024-03-25 19:53:02 -0300

    :sparkles: atividade 1

--- algoritmos/aula06/exercicio03.py
+++ algoritmos/aula06/exercicio03.py
@@ -9,3 +9,11 @@ quantidade_latas = (area_pintada/3)/litros_lata # considerando que cada litro pi
 
 print(f"A área a ser pintada do aposento é de {area_pintada}m², sendo necessarias "
       f"{quantidade_latas} latas de tinta para realizar a pintura nas paredes.")
+
+# Teste
+# largura = 10, comprimento = 20, litros por lata: 5
+# area_paredes = (10*2.8)*2 + (20*2.8)*2 = 56 + 112 = 168
+# area porta = 1,68
+# area a ser pintada = 166,32
+# quantidade latas = 1 litros pinta 3m². logo sera necessarios 166,32/3 litros = 55,44
+# cada lata tem 5 litros, logo serão necessárias 11.088 latas de tinta
