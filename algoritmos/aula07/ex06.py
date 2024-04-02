8b5f243f8d37312bef0c1ba3c3ccd738a8eb6fa8
nordivi <victorw.nordi@icloud.com>
2024-04-01 22:10:47 -0300

    :sparkles: ex06

--- algoritmos/aula07/ex06.py
+++ algoritmos/aula07/ex06.py
@@ -1,19 +1,23 @@
 ultimo_numero = 1
 penultimo_numero = 1
-for i in range(8):
+print('1: 1')
+print('2: 1')
+for i in range(48):
     s = ultimo_numero + penultimo_numero
+    print(f'{i+3}: {s}')
     penultimo_numero = ultimo_numero
     ultimo_numero = s
 
-print("O décimo número da sequência fibonacci é: ", s)
 
 ultimo_numero = 1
 penultimo_numero = 1
+print('1: 1')
+print('2: 1')
 i=0
-while i != 8:
+while i != 48:
     s = ultimo_numero + penultimo_numero
+    print(f'{i + 3}: {s}')
     penultimo_numero = ultimo_numero
     ultimo_numero = s
     i+=1
 
-print("O décimo número da sequência fibonacci é: ", s)
