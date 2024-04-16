3ff43fdeca3bb270b1fc259d79010899b233388f
nordivi <victorw.nordi@icloud.com>
2024-04-15 21:37:41 -0300

    :sparkles: exercicios aula11

--- algoritmos/aula11/ex03c.py
+++ algoritmos/aula11/ex03c.py
@@ -3,4 +3,4 @@ l2 = [int(input(f"{i+1}º valor da lista_2: ")) for i in range(5)]
 uniao = set(l1).union(l2)
 print("Lista 1: ", l1)
 print("Lista 2: ", l2)
-print("\nConjunto da união entre as duas listas:", uniao)
+print("\nUnião entre lista_1 e lista_2:", uniao)
