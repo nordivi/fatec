d70c838e724cd9bcc32fded8c67876d65973d677
nordivi <victorw.nordi@icloud.com>
2024-08-20 22:02:39 -0300

    :sparkles: left join

--- sem-2/dados-relacional/insert.sql
+++ sem-2/dados-relacional/insert.sql
@@ -5,5 +5,14 @@ insert into cidade values ('1', 'Votorantim', 'SP')
 insert into cidade (Codcidade, Nomecidade, Cdestado)
 values ('2', 'Sorocaba', 'SP')
 
-select * from cidade
+
+insert into estado values ('RJ', 'Rio de Janeiro')
+
+insert into cidade values ('3', 'Rio de Janeiro', 'RJ')
+
+insert into cidade (Codcidade, Nomecidade, Cdestado)
+values ('4','Copacabana', 'RJ')
+
+
+select Nomecidade, cidade.cdestado, Nomeestado from cidade
 left join estado on cidade.cdestado = estado.cdestado
