55b1de0ddace30ab4524f6843c65ed549487a1ed
nordivi <victorw.nordi@icloud.com>
2024-08-29 21:59:57 -0300

    :sparkles: header

--- sem-2/dev-web-ii/angular/angular/src/app/main/main.component.ts
+++ sem-2/dev-web-ii/angular/angular/src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
